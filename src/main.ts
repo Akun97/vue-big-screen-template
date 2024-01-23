c053d7101f486174ad1d93033bda028a91928967
Akun97 <17759735780@163.com>
2024-01-23 10:15:11 +0800

    2024年1月23日10:15:14

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
-import '@/styles/index.css';
+import '@/styles/index.scss';
 import VueFullscreen from 'vue-fullscreen';
 
 const app = createApp(App);
